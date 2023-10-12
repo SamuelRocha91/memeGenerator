b2921a0cf1ddd07e8b874c57ec9e71228e2fb3cc
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-12 20:35:17 -0300

    adiciona input de insercao de arquivo e logica de carregamento de imagem

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-const input = document.getElementById('text-input');
+const inputText = document.getElementById('text-input');
 const p = document.getElementById('meme-text');
+const inputFile = document.getElementById('meme-insert');
+const img = document.querySelector('img');
 
 function evento() {
-  return input.addEventListener('input', (event) => {
+  return inputText.addEventListener('input', (event) => {
     p.innerText = event.target.value;
+  });
+}
+
+function insertImage() {
+  return inputFile.addEventListener('change', (event) => {
+    console.log(event);
+    img.src = URL.createObjectURL(event.target.files[0]);
   });
 }
 
 window.onload = () => {
   evento();
+  insertImage();
 };

2faa29447379858ad315c288ed0c895aaa045bd0
SamuelRocha91 <samuel_sr@hotmail.com.br>
2023-10-12 21:24:18 -0300

    cria botoes que adicionam borda ao container

--- main.js
+++ main.js
@@ -2,6 +2,32 @@ const inputText = document.getElementById('text-input');
 const p = document.getElementById('meme-text');
 const inputFile = document.getElementById('meme-insert');
 const img = document.querySelector('img');
+const fire = document.getElementById('fire');
+const water = document.getElementById('water');
+const earth = document.getElementById('earth');
+const MEME_CONTAINER = 'meme-image-container';
+
+function insertBorderColor() {
+  const container = document.getElementById(MEME_CONTAINER);
+  document.getElementById('fire').addEventListener('click', () => {
+    container.style.removeProperty('backgroundCollor');
+    document.getElementById('meme-image-container').style.border = '3px dashed rgb(255, 0, 0)';
+  });
+  document.getElementById('earth').addEventListener('click', () => {
+    container.style.removeProperty('backgroundCollor');
+    document.getElementById(MEME_CONTAINER).style.border = '6px groove rgb(0, 128, 0)';
+  });
+  document.getElementById('water').addEventListener('click', () => {
+    container.style.removeProperty('backgroundCollor');
+    document.getElementById(MEME_CONTAINER).style.border = '5px double rgb(0, 0, 255)';
+  });
+}
+
+function addColor() {
+  document.getElementById('fire').style.backgroundColor = 'rgb(255 0 0)';
+  document.getElementById('water').style.backgroundColor = 'rgb(0 0 255)';
+  document.getElementById('earth').style.backgroundColor = 'rgb(0, 128, 0)';
+}
 
 function evento() {
   return inputText.addEventListener('input', (event) => {
@@ -19,4 +45,6 @@ function insertImage() {
 window.onload = () => {
   evento();
   insertImage();
+  addColor();
+  insertBorderColor();
 };
